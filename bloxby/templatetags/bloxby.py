bdede95e632a66f83a7b3cd3b8127a3b9e2c5f71
Damilola Adeyemi <neefemee@gmail.com>
2020-04-08 23:50:28 +0100

    make link dead if there is no bloxby id attached to user

--- bloxby/templatetags/bloxby.py
+++ bloxby/templatetags/bloxby.py
@@ -8,9 +8,9 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def user_builder_dashboard(context):
     user = context['request'].user
-    if user.is_authenticated:
-        try:
+    try:
+        if user.is_authenticated and user.userbridge.bloxby_id:
             return user.userbridge.dashboard_url
-        except UserBridge.DoesNotExist:
-            pass
+    except UserBridge.DoesNotExist:
+        pass
     return '#'
