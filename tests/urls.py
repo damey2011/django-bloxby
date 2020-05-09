58af44932d91713a5d0c461f2d3d4d83a80c9758
Damilola Adeyemi <neefemee@gmail.com>
2020-05-09 12:25:06 +0100

    fix admin

--- tests/urls.py
+++ tests/urls.py
@@ -5,8 +5,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin')),
     path('admin/', admin.site.urls),
     path('bloxby/', include('djbloxby.bloxby.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
