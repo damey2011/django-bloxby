a3f3e595ad6f55126ac9a14b885354d7a314d14b
Damilola Adeyemi <neefemee@gmail.com>
2020-05-09 12:11:18 +0100

    remove activation of virtualenv and replace with direct path

--- setup_ftp_server.sh
+++ setup_ftp_server.sh
@@ -2,8 +2,7 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install nodejs
 npm install pm2@latest -g
 virtualenv env -p python3
-pip install -r requirements.txt
-source env/bin/activate
-python manage.py migrate
+env/bin/pip install -r requirements.txt
+env/bin/python manage.py migrate
 sudo ufw allow 21/tcp
 sudo ufw allow 8000/tcp
